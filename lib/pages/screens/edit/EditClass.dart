54f40cacffe63f17c12ce4be37f34390c7c2a5e4
Bronziel Gold <morgangiermogrimes@gmail.com>
2023-06-08 16:57:00 +0200

    this shit sucks

---
+++ lib/pages/screens/edit/EditClass.dart
@@ -0,0 +1,325 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:path/path.dart' as Path;
+import 'package:flutter/services.dart';
+import 'EditRecipe.dart';
+
+class EditIngredientsBox extends StatefulWidget {
+  const EditIngredientsBox({
+    Key? key,
+    required this.newRecipe,
+    required this.formKey,
+  }) : super(key: key);
+
+  final NewRecipe newRecipe;
+  final GlobalKey<FormState> formKey;
+
+  @override
+  _EditIngredientsBoxState createState() => _EditIngredientsBoxState();
+}
+
+class _EditIngredientsBoxState extends State<EditIngredientsBox> {
+  List<Map<String, dynamic>> _ingredients = [];
+  List<Widget> _ingredientWidgets = [];
+
+  void _addIngredient() {
+    Map<String, dynamic> newIngredient = {'name': '', 'amount': '', 'unit': ''};
+    _ingredients.add(newIngredient);
+
+    setState(() {
+      _ingredientWidgets.add(
+        Row(
+          children: [
+            Flexible(
+              child: TextFormField(
+                decoration: InputDecoration(hintText: 'Enter ingredient name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an ingredient name';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  newIngredient['name'] = value!;
+                  widget.newRecipe.ingredients = _ingredients;
+                },
+              ),
+            ),
+            SizedBox(width: 8.0),
+            Flexible(
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+                decoration:
+                    InputDecoration(hintText: 'Enter ingredient amount'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an ingredient amount';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  if (value != null && value.isNotEmpty) {
+                    newIngredient['amount'] = int.parse(value);
+                  }
+                },
+              ),
+            ),
+            const SizedBox(width: 8.0),
+            Flexible(
+              child: TextFormField(
+                decoration: InputDecoration(hintText: 'Enter ingredient unit'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an ingredient unit';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  newIngredient['unit'] = value!;
+                  widget.newRecipe.ingredients = _ingredients;
+                },
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _addIngredient,
+              child: const Text('Add another ingredient'),
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  _ingredients.remove(newIngredient);
+                  _ingredientWidgets.removeLast();
+                });
+              },
+              icon: Icon(Icons.delete),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Start with one ingredient
+    _addIngredient();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: Form(
+            key: widget.formKey,
+            child: Column(children: _ingredientWidgets),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    // Save the ingredients to the recipe before disposing
+    widget.newRecipe.ingredients = _ingredients;
+    super.dispose();
+  }
+}
+
+class EditEquipmentBox extends StatelessWidget {
+  const EditEquipmentBox({
+    Key? key,
+    required this.newRecipe,
+  }) : super(key: key);
+
+  final NewRecipe newRecipe;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(hintText: 'Enter equipment (one per line)'),
+      maxLines: null,
+      keyboardType: TextInputType.multiline,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter at least one equipment';
+        }
+        return null;
+      },
+      onSaved: (value) {
+        newRecipe.equipment = value!.split('\n');
+      },
+    );
+  }
+}
+
+class EditStepsBox extends StatelessWidget {
+  const EditStepsBox({
+    Key? key,
+    required NewRecipe newRecipe,
+  })  : _newRecipe = newRecipe,
+        super(key: key);
+
+  final NewRecipe _newRecipe;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(hintText: 'Enter steps (one per line)'),
+      maxLines: null,
+      keyboardType: TextInputType.multiline,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter at least one step';
+        }
+        return null;
+      },
+      onSaved: (value) {
+        _newRecipe.steps = value!.split('\n');
+      },
+    );
+  }
+}
+
+class EditPortionsizeBox extends StatelessWidget {
+  const EditPortionsizeBox({
+    Key? key,
+    required this.newRecipe,
+  }) : super(key: key);
+
+  final NewRecipe newRecipe;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      inputFormatters: <TextInputFormatter>[
+        FilteringTextInputFormatter.digitsOnly
+      ],
+      decoration: const InputDecoration(hintText: 'Enter portion size'),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a portion size';
+        }
+        return null;
+      },
+      onSaved: (value) => newRecipe.portionSize = int.parse(value ?? '0'),
+    );
+  }
+}
+
+class EditDescriptionBox extends StatelessWidget {
+  const EditDescriptionBox({
+    Key? key,
+    required this.newRecipe,
+  }) : super(key: key);
+
+  final NewRecipe newRecipe;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: 'Enter description'),
+      maxLines: null, // Allow multiple lines
+      keyboardType: TextInputType.multiline, // Enable multiline input
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a description';
+        }
+        return null;
+      },
+      onSaved: (value) => newRecipe.description = value!,
+    );
+  }
+}
+
+class EditTitelBox extends StatelessWidget {
+  const EditTitelBox({
+    super.key,
+    required NewRecipe newRecipe,
+  }) : _newRecipe = newRecipe;
+
+  final NewRecipe _newRecipe;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: 'Enter recipe title'),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a title';
+        }
+        return null;
+      },
+      onSaved: (value) => _newRecipe.title = value!,
+    );
+  }
+}
+
+class CookTimeBox extends StatelessWidget {
+  const CookTimeBox({
+    Key? key,
+    required this.newRecipe,
+  }) : super(key: key);
+
+  final NewRecipe newRecipe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          child: TextFormField(
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+            decoration: const InputDecoration(hintText: 'Prep Time (min)'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a prep time';
+              }
+              return null;
+            },
+            onSaved: (value) {
+              if (value != null && value.isNotEmpty) {
+                newRecipe.prepTime = int.parse(value);
+              }
+            },
+          ),
+        ),
+        const SizedBox(width: 8.0),
+        Flexible(
+          child: TextFormField(
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+            decoration: const InputDecoration(hintText: 'Total Time (min)'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a total time';
+              }
+              return null;
+            },
+            onSaved: (value) {
+              if (value != null && value.isNotEmpty) {
+                newRecipe.totalTime = int.parse(value);
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/pages/screens/edit/EditRecipe.dart
@@ -0,0 +1,174 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:path/path.dart' as Path;
+import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
+
+import '../../recipes/getrecipe.dart';
+import '../../recipes/recipeLayout.dart';
+import 'EditClass.dart';
+import 'EditImage.dart';
+
+class NewRecipe {
+  String title = '';
+  String description = '';
+  int portionSize = 1;
+  int prepTime = 0; // new field
+  int totalTime = 0; // new field
+  List<String> steps = [];
+  List<String> equipment = []; // new field
+  List<Map<String, dynamic>> ingredients =
+      []; // updated to dynamic to include units
+  List<XFile>? images; // updated to support multiple images
+}
+
+class EditRecipeForm extends StatefulWidget {
+  final VoidCallback onRecipeSaved;
+  final Recipe? recipe;
+
+  const EditRecipeForm({Key? key, required this.onRecipeSaved, this.recipe})
+      : super(key: key);
+
+  @override
+  _EditRecipeFormState createState() => _EditRecipeFormState();
+}
+
+class _EditRecipeFormState extends State<EditRecipeForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _ingredientsFormKey = GlobalKey<FormState>();
+  final NewRecipe _newRecipe = NewRecipe();
+  // Dispose of the selected image
+  void _deleteImage() {
+    setState(() {
+      _newRecipe.images = null;
+    });
+  }
+
+// For selecting multiple images from the user's device
+  Future<void> _pickImages() async {
+    final ImagePicker _picker = ImagePicker();
+    final List<XFile>? images = await _picker.pickMultiImage();
+
+    setState(() {
+      _newRecipe.images = images;
+    });
+  }
+
+  Future<List<String>> _uploadImagesToFirebase(List<XFile> images) async {
+    List<String> imageUrls = [];
+    for (var imageFile in images) {
+      String fileName = Path.basename(imageFile.path);
+      Uint8List data = await imageFile.readAsBytes();
+
+      final metadata = firebase_storage.SettableMetadata(
+        contentType: 'image/jpeg', // Set content type explicitly as image/jpeg
+      );
+
+      firebase_storage.Reference ref =
+          firebase_storage.FirebaseStorage.instance.ref('/images/$fileName');
+
+      firebase_storage.UploadTask uploadTask = ref.putData(data, metadata);
+      await uploadTask;
+
+      String imageUrl = await ref.getDownloadURL();
+      imageUrls.add(imageUrl);
+    }
+    return imageUrls;
+  }
+
+  Future<void> _uploadRecipeToFirestore(
+      NewRecipe recipe, List<String> imageUrls) async {
+    await FirebaseFirestore.instance.collection('recipes').add({
+      'title': recipe.title,
+      'description': recipe.description,
+      'portionSize': recipe.portionSize,
+      'steps': recipe.steps,
+      'ingredients': recipe.ingredients,
+      'imageUrls': imageUrls, // updated to pass a list of URLs
+      'prepTime': recipe.prepTime,
+      'totalTime': recipe.totalTime,
+      'equipment': recipe.equipment,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: [
+                  Flexible(child: EditTitelBox(newRecipe: _newRecipe)),
+                  const SizedBox(width: 16.0),
+                  Flexible(child: EditPortionsizeBox(newRecipe: _newRecipe)),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              CookTimeBox(newRecipe: _newRecipe),
+              const SizedBox(height: 16.0),
+              EditDescriptionBox(newRecipe: _newRecipe),
+              const SizedBox(height: 16.0),
+              EditEquipmentBox(newRecipe: _newRecipe),
+              const SizedBox(height: 16.0),
+              EditStepsBox(newRecipe: _newRecipe),
+              const SizedBox(height: 16.0),
+              EditIngredientsBox(
+                newRecipe: _newRecipe,
+                formKey: _ingredientsFormKey,
+              ),
+
+              // Continue with other fields in similar way: description, portionSize, steps, etc.
+              // ...
+
+              // Image picker and delete image buttons
+              ImagePickerWidget(
+                onImagesPicked: (images) {
+                  setState(() {
+                    _newRecipe.images = images;
+                  });
+                },
+                initialImages: _newRecipe.images,
+              ),
+
+              // Submit button
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate() &&
+                      _ingredientsFormKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    _ingredientsFormKey.currentState!.save();
+                    List<String> imageUrls = [];
+                    if (_newRecipe.images != null &&
+                        _newRecipe.images!.isNotEmpty) {
+                      imageUrls =
+                          await _uploadImagesToFirebase(_newRecipe.images!);
+                    }
+                    await _uploadRecipeToFirestore(_newRecipe, imageUrls);
+                    Navigator.pop(context); // close the form
+                    widget.onRecipeSaved(); // notify that recipe has been saved
+                  }
+                },
+                child: const Text('Submit Recipe'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _newRecipe.images = [];
+                  });
+                },
+                child: Icon(Icons.delete),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
