5f690ee7f6379d15523683ee93507b27d294f576
Bronziel Gold <morgangiermogrimes@gmail.com>
2023-06-13 16:22:01 +0200

    prep for recipelistpage

--- lib/pages/recipes/RecipePages/recipelistpage.dart
+++ lib/pages/recipes/RecipePages/recipelistpage.dart
@@ -4,8 +4,7 @@ import '../../../visualview/recipestuff/RecipeLook/recipeLayout.dart';
 import 'RecipeViewPage.dart';
 import '../getrecipe.dart' show fetchRecipes, deleteRecipe;
 import '../../../visualview/recipestuff/formwidget/recipe_form.dart';
-
-import '../../../visualview/recipestuff/edit/EditRecipe.dart';
+import 'recipemenubutton.dart';
 
 class RecipelistPage extends StatefulWidget {
   const RecipelistPage({Key? key}) : super(key: key);
@@ -95,85 +94,6 @@ class _RecipelistPageState extends State<RecipelistPage> {
           );
         },
       ),
-    );
-  }
-}
-
-class RecipeActionsPopup extends StatelessWidget {
-  final Recipe recipe;
-  final VoidCallback onRecipeAction;
-
-  const RecipeActionsPopup(
-      {required this.recipe, required this.onRecipeAction});
-
-  Future<void> _deleteRecipe(BuildContext context) async {
-    bool? shouldDelete = await showDialog<bool>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Delete Recipe'),
-          content: const Text('Are you sure you want to delete this recipe?'),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.pop(context, false);
-              },
-            ),
-            TextButton(
-              child: const Text('Delete'),
-              onPressed: () {
-                Navigator.pop(context, true);
-              },
-            ),
-          ],
-        );
-      },
-    );
-
-    if (shouldDelete == true) {
-      await deleteRecipe(recipe);
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('The recipe has been deleted')),
-      );
-      onRecipeAction();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return PopupMenuButton<int>(
-      itemBuilder: (context) => [
-        const PopupMenuItem(
-          value: 1,
-          child: ListTile(
-            leading: Icon(Icons.edit),
-            title: Text('Edit'),
-          ),
-        ),
-        const PopupMenuItem(
-          value: 2,
-          child: ListTile(
-            leading: Icon(Icons.delete),
-            title: Text('Delete'),
-          ),
-        ),
-      ],
-      onSelected: (value) {
-        if (value == 1) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => EditRecipeForm(
-                recipe: recipe,
-                onRecipeSaved: onRecipeAction,
-              ),
-            ),
-          );
-        } else if (value == 2) {
-          _deleteRecipe(context);
-        }
-      },
     );
   }
 }
