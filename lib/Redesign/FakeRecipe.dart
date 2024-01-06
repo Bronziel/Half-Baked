f4b67116b8a57bb6b4bdc570c351693f8f71cc3f
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-01-06 17:33:19 +0100

    bilder kebab

--- lib/Redesign/FakeRecipe.dart
+++ lib/Redesign/FakeRecipe.dart
@@ -8,7 +8,7 @@ class NewDesignPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100.0),
+        preferredSize: Size.fromHeight(90.0),
         child: AppBar(
           flexibleSpace: SafeArea(
             child: Stack(
@@ -34,10 +34,162 @@ class NewDesignPage extends StatelessWidget {
           ),
         ),
       ),
-      body: const Center(
-        child: Text(
-          'Welcome to the Landing Page!',
-          style: TextStyle(fontSize: 24),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  SizedBox(
+                    height: 400,
+                    width: 410,
+                    child: Card(
+                        // Add other properties of Card if needed
+                        ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  SizedBox(
+                    height: 400,
+                    width: 1085,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            10), // Define the border radius here
+                      ),
+                      child: ClipRRect(
+                        borderRadius:
+                            BorderRadius.circular(10), // Same radius as Card
+                        child: Image.asset(
+                          'images/new/kebabrulle.jpg',
+                          fit: BoxFit.cover, // Adjust the fit as needed
+                        ),
+                      ),
+                      // Add other properties of Card if needed
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 70,
+                        width: 70,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                10), // Define the border radius here
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(
+                                10), // Same radius as Card
+                            child: Image.asset(
+                              'images/new/kebabrulle.jpg',
+                              fit: BoxFit.cover, // Adjust the fit as needed
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      SizedBox(
+                        height: 70,
+                        width: 70,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                10), // Define the border radius here
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(
+                                10), // Same radius as Card
+                            child: Image.asset(
+                              'images/new/kebab1.jpeg',
+                              fit: BoxFit.cover, // Adjust the fit as needed
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      SizedBox(
+                        height: 70,
+                        width: 70,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                10), // Define the border radius here
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(
+                                10), // Same radius as Card
+                            child: Image.asset(
+                              'images/new/kebab3.jpg',
+                              fit: BoxFit.cover, // Adjust the fit as needed
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      SizedBox(
+                        height: 70,
+                        width: 70,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                10), // Define the border radius here
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(
+                                10), // Same radius as Card
+                            child: Image.asset(
+                              'images/new/kebabrulle.jpg',
+                              fit: BoxFit.cover, // Adjust the fit as needed
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 40,
+                  ),
+                  SizedBox(
+                    width: 410,
+                    height: 463,
+                    child: Card(),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  SizedBox(
+                    width: 1175,
+                    height: 474,
+                    child: Card(),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
