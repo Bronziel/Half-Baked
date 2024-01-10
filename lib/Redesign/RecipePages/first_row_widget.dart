79f02e540a42b3b1881ae0cc67f15a44aaaff642
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-01-10 19:11:28 +0100

    added the images in a continaer

--- lib/Redesign/RecipePages/first_row_widget.dart
+++ lib/Redesign/RecipePages/first_row_widget.dart
@@ -15,105 +15,122 @@ class FirstRowWidget extends StatelessWidget {
         const Widht40wallWidget(),
         const IntroductionWidget(),
         const Width20StandardWidget(),
-        SizedBox(
-          height: 400,
-          width: 1085,
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius:
-                  BorderRadius.circular(10), // Define the border radius here
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10), // Same radius as Card
-              child: Image.asset(
-                'images/new/kebabrulle.jpg',
-                fit: BoxFit.cover, // Adjust the fit as needed
-              ),
-            ),
-            // Add other properties of Card if needed
-          ),
-        ),
-        const Width20StandardWidget(),
-        Column(
-          children: [
-            SizedBox(
-              height: 70,
-              width: 70,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                      10), // Define the border radius here
-                ),
-                child: ClipRRect(
-                  borderRadius:
-                      BorderRadius.circular(10), // Same radius as Card
-                  child: Image.asset(
-                    'images/new/kebabrulle.jpg',
-                    fit: BoxFit.cover, // Adjust the fit as needed
-                  ),
-                ),
-              ),
-            ),
-            const Height40StandardWidget(),
-            SizedBox(
-              height: 70,
-              width: 70,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                      10), // Define the border radius here
-                ),
-                child: ClipRRect(
-                  borderRadius:
-                      BorderRadius.circular(10), // Same radius as Card
-                  child: Image.asset(
-                    'images/new/kebab1.jpeg',
-                    fit: BoxFit.cover, // Adjust the fit as needed
-                  ),
-                ),
-              ),
-            ),
-            const Height40StandardWidget(),
-            SizedBox(
-              height: 70,
-              width: 70,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                      10), // Define the border radius here
-                ),
-                child: ClipRRect(
-                  borderRadius:
-                      BorderRadius.circular(10), // Same radius as Card
-                  child: Image.asset(
-                    'images/new/kebab3.jpg',
-                    fit: BoxFit.cover, // Adjust the fit as needed
-                  ),
-                ),
-              ),
-            ),
-            const Height40StandardWidget(),
-            SizedBox(
-              height: 70,
-              width: 70,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                      10), // Define the border radius here
-                ),
-                child: ClipRRect(
-                  borderRadius:
-                      BorderRadius.circular(10), // Same radius as Card
-                  child: Image.asset(
-                    'images/new/kebabrulle.jpg',
-                    fit: BoxFit.cover, // Adjust the fit as needed
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+        MainImagePlusBoxes(),
       ],
+    );
+  }
+}
+
+class MainImagePlusBoxes extends StatelessWidget {
+  const MainImagePlusBoxes({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          SizedBox(
+            height: 400,
+            width: 1085,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.circular(10), // Define the border radius here
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10), // Same radius as Card
+                child: Image.asset(
+                  'images/new/kebabrulle.jpg',
+                  fit: BoxFit.cover, // Adjust the fit as needed
+                ),
+              ),
+              // Add other properties of Card if needed
+            ),
+          ),
+          const Width20StandardWidget(),
+          Column(
+            children: [
+              SizedBox(
+                height: 70,
+                width: 70,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        10), // Define the border radius here
+                  ),
+                  child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.circular(10), // Same radius as Card
+                    child: Image.asset(
+                      'images/new/kebabrulle.jpg',
+                      fit: BoxFit.cover, // Adjust the fit as needed
+                    ),
+                  ),
+                ),
+              ),
+              const Height40StandardWidget(),
+              SizedBox(
+                height: 70,
+                width: 70,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        10), // Define the border radius here
+                  ),
+                  child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.circular(10), // Same radius as Card
+                    child: Image.asset(
+                      'images/new/kebab1.jpeg',
+                      fit: BoxFit.cover, // Adjust the fit as needed
+                    ),
+                  ),
+                ),
+              ),
+              const Height40StandardWidget(),
+              SizedBox(
+                height: 70,
+                width: 70,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        10), // Define the border radius here
+                  ),
+                  child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.circular(10), // Same radius as Card
+                    child: Image.asset(
+                      'images/new/kebab3.jpg',
+                      fit: BoxFit.cover, // Adjust the fit as needed
+                    ),
+                  ),
+                ),
+              ),
+              const Height40StandardWidget(),
+              SizedBox(
+                height: 70,
+                width: 70,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        10), // Define the border radius here
+                  ),
+                  child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.circular(10), // Same radius as Card
+                    child: Image.asset(
+                      'images/new/kebabrulle.jpg',
+                      fit: BoxFit.cover, // Adjust the fit as needed
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
