89438e9a9114be2ebe51b6b61e6ca023575c8ee1
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-01-14 14:19:10 +0100

    gis

--- lib/Redesign/RecipePages/Recipe/base_recipe.dart
+++ lib/Redesign/RecipePages/Recipe/base_recipe.dart
@@ -6,7 +6,7 @@ import 'FirstRow/first_row_widget.dart';
 import 'second_row_widget.dart';
 
 class NewDesignPage extends StatelessWidget {
-  const NewDesignPage({Key? key}) : super(key: key);
+  const NewDesignPage({super.key});
 
   @override
   Widget build(BuildContext context) {
