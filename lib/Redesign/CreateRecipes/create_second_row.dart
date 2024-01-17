baaaa9c419a64efcb50e468733ae86ce78122cd1
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-01-17 11:28:53 +0100

    okey

--- lib/Redesign/CreateRecipes/create_second_row.dart
+++ lib/Redesign/CreateRecipes/create_second_row.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../components/sizedboxes.dart';
-import 'popups.dart';
+
 import 'package:flutter/services.dart';
 import 'style.dart';
 import 'style_objects.dart';
--- lib/Redesign/Testin_components/doneshowcase.dart
+++ lib/Redesign/Testin_components/doneshowcase.dart
@@ -178,3 +178,35 @@ class listButton extends StatelessWidget {
     );
   }
 }
+
+class MyownButton extends StatelessWidget {
+  final String buttonText;
+  final Color buttonColor;
+
+  const MyownButton({
+    super.key,
+    this.buttonText = 'list',
+    this.buttonColor = const Color.fromARGB(255, 12, 122, 225),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const DropdownMenuExample()),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.all(16.0),
+        minimumSize: const Size(double.infinity, 64.0),
+        backgroundColor: buttonColor,
+      ),
+      child: Text(
+        buttonText,
+        style: const TextStyle(fontSize: 36, color: Colors.white),
+      ),
+    );
+  }
+}
