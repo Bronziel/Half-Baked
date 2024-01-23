3ddd769a1c496189b8f0e5a58b2b04be39f812a3
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-01-23 14:30:08 +0100

    aded new style file

--- lib/Redesign/components/styles/textstyle.dart
+++ lib/Redesign/components/styles/textstyle.dart
@@ -1,8 +1,9 @@
 //popuptextfields use 20 inter.
 //popuptitle use monserrat semibodl 20
 //hint text textfield inter 20,
+
 //new design description 24 fontweight bold
-//new design title Pacifico 40 
+//new design title Pacifico 40
 //new design portion time font size 24 fontweight w.700
 //new design steps number Pacifico 40 blue
 //new desing steps text Paprika 24 black
@@ -10,3 +11,85 @@
 //new desing ingirindet box title 24 FontWeight.w700,
 
 //showcase text
+
+import 'package:flutter/material.dart';
+
+class StyleTextRecipe {
+  static TextStyle titelrecipe() {
+    return const TextStyle(
+      fontFamily: 'Pacifico',
+      fontSize: 40,
+    );
+  }
+
+  static TextStyle descriptionrecipe() {
+    return const TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 24,
+    );
+  }
+
+  static TextStyle porttotalrecipe() {
+    return const TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 24,
+    );
+  }
+
+  static TextStyle ingboxtitel() {
+    return const TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 24,
+    );
+  }
+
+  static TextStyle ingbox() {
+    return const TextStyle(
+      fontWeight: FontWeight.w700,
+      fontSize: 24,
+    );
+  }
+
+  static TextStyle stepsnumber() {
+    return const TextStyle(
+      fontFamily: 'Pacifico',
+      fontSize: 40,
+    );
+  }
+
+  static TextStyle stepstext() {
+    return const TextStyle(
+      fontFamily: 'Paprika',
+      fontSize: 24,
+    );
+  }
+}
+
+//popupboxes
+class TextPopStyle {
+  static TextStyle poptitle() {
+    return const TextStyle(
+      fontFamily: 'Montserrat Semibold',
+      fontSize: 20,
+    );
+  }
+
+  static TextStyle poptextfield() {
+    return const TextStyle(
+      fontFamily: 'inter',
+      fontSize: 20,
+    );
+  }
+
+  static TextStyle pophinttext() {
+    return const TextStyle(
+      fontFamily: 'inter',
+      fontSize: 20,
+    );
+  }
+}
+
+
+//popuptextfields use 20 inter.
+//popuptitle use monserrat semibodl 20
+//hint text textfield inter 20,
