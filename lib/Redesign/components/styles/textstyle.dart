e46994b9b8bb017f20f940cf1c4d9aa9c7dfec92
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-01-23 14:18:09 +0100

    checked all fonts

---
+++ lib/Redesign/components/styles/textstyle.dart
@@ -0,0 +1,12 @@
+//popuptextfields use 20 inter.
+//popuptitle use monserrat semibodl 20
+//hint text textfield inter 20,
+//new design description 24 fontweight bold
+//new design title Pacifico 40 
+//new design portion time font size 24 fontweight w.700
+//new design steps number Pacifico 40 blue
+//new desing steps text Paprika 24 black
+//new desing ingridnet box fontsize 24 fontweight w.700
+//new desing ingirindet box title 24 FontWeight.w700,
+
+//showcase text
