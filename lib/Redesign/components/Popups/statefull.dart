330d54e1d959616d8ba6293ff89db94fa355b5ed
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-02-18 16:13:14 +0100

    stepstextfield

--- lib/Redesign/components/Popups/statefull.dart
+++ lib/Redesign/components/Popups/statefull.dart
@@ -122,3 +122,38 @@ class _DescriptionTextFieldState extends State<DescriptionTextField> {
     );
   }
 }
+
+//steps textfield for saving new steps.
+class StepsTextField extends StatefulWidget {
+  final String labeltext;
+  const StepsTextField({
+    required this.labeltext,
+    super.key,
+  });
+
+  @override
+  State<StepsTextField> createState() => _StepsTextFieldState();
+}
+
+class _StepsTextFieldState extends State<StepsTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 10,
+      left: 10,
+      top: 45,
+      right: 100, // Adjusted to ensure the TextField is more square-like
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: widget.labeltext,
+          labelStyle: StyleUtils.labelstylepop(),
+          border: const OutlineInputBorder(),
+          enabledBorder: StyleUtils.enabledborderstyle(),
+        ),
+        style: StyleUtils.textfieldstylepop(),
+        maxLines: null, // Allows for unlimited lines
+        minLines: 7, // Set this to a desired initial size
+      ),
+    );
+  }
+}
