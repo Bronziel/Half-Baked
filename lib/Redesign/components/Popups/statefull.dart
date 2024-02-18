c2f7ff6c5259a5bb5790075600b44bde5f397ec9
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-02-18 16:08:46 +0100

    small textfield done

--- lib/Redesign/components/Popups/statefull.dart
+++ lib/Redesign/components/Popups/statefull.dart
@@ -45,3 +45,43 @@ class _NumberTextFieldState extends State<NumberTextField> {
     );
   }
 }
+
+//litet textfield använd av title stepstitle o andra en rads fält
+class TitleTextField extends StatefulWidget {
+  final String labelText;
+  final String hintText;
+  final double bottom;
+  const TitleTextField({
+    required this.labelText,
+    this.hintText = '',
+    this.bottom = 10,
+    super.key,
+  });
+
+  @override
+  State<TitleTextField> createState() => _TitleTextFieldState();
+}
+
+class _TitleTextFieldState extends State<TitleTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      // Set the position for the TextField
+      bottom: widget.bottom, // Adjust these values as needed
+      left: 10, // Adjust these values as needed
+      right: 100,
+
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: widget.labelText,
+          labelStyle: StyleUtils.labelstylepop(),
+          hintText: widget.hintText,
+          hintStyle: StyleUtils.hintstylepop(),
+          border: const OutlineInputBorder(),
+          enabledBorder: StyleUtils.enabledborderstyle(),
+        ),
+        style: StyleUtils.textfieldstylepop(),
+      ),
+    );
+  }
+}
