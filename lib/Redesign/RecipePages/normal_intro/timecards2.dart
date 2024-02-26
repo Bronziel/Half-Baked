242e71313874562ba7fcf08f377b1121d20b6f08
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-02-26 20:19:10 +0100

    timecard

---
+++ lib/Redesign/RecipePages/normal_intro/timecards2.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../..//components/currents/cards/fixedtimecard.dart';
+
+//displaytimes(title: title, time: time)
+//addtimes(titleadd: titleadd),
+class Displaytimesnow extends StatelessWidget {
+  const Displaytimesnow({
+    super.key,
+    required this.title,
+    required this.time,
+  });
+
+  final String title;
+  final String time;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 290,
+      height: 50,
+      child: Card(
+        color: const Color(0xffd9d9d9),
+        child: Row(
+          children: [
+            const SizedBox(width: 9),
+            const Clockimg(),
+            const SizedBox(width: 5),
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const SizedBox(width: 8),
+            Text(
+              time,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const SizedBox(width: 1),
+            const Text(
+              "min",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
