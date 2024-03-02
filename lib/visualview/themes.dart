69b0e1b57e910bace9613d4bd9669040a24e9aeb
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-03-02 16:57:32 +0100

    made background white

--- lib/visualview/themes.dart
+++ lib/visualview/themes.dart
@@ -4,7 +4,8 @@ class AppThemes {
   static final ThemeData defaultTheme = ThemeData(
     appBarTheme: const AppBarTheme(
       // Customize the AppBar theme properties
-      color: Color(0xFF9896F1), // Set the background color of the AppBar
+      color: Color.fromARGB(
+          255, 233, 233, 238), // Set the background color of the AppBar
       iconTheme: IconThemeData(
           color: Colors
               .white), // Set the color of the AppBar icons (e.g., back button)
@@ -12,8 +13,8 @@ class AppThemes {
           TextStyle(color: Colors.white), // Set the color of the AppBar title
     ),
     // Define other properties of the default theme if needed
-    primaryColor: const Color(0xFF9896F1),
-    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.red),
+    primaryColor: Color.fromARGB(255, 242, 242, 244),
+
     // Other theme properties...
   );
 
