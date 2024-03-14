0aad825ac1a9590574865bf19b2fe0f69c378d11
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-03-14 10:28:51 +0100

    image pikcers

--- lib/Redesign/components/currents/imgholders/image_create_hold/imagepicker.dart
+++ lib/Redesign/components/currents/imgholders/image_create_hold/imagepicker.dart
@@ -3,6 +3,51 @@ import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 
 //1:image pikcer
+class ImagePickerRedone extends StatefulWidget {
+  const ImagePickerRedone({super.key});
 
+  @override
+  State<ImagePickerRedone> createState() => _ImagePickerRedoneState();
+}
+
+class _ImagePickerRedoneState extends State<ImagePickerRedone> {
+  File? _image;
+
+  Future getImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 800,
+        height: 800,
+        child: Row(
+          children: [
+            _image == null
+                ? Column(
+                    children: [
+                      IconButton(
+                        onPressed: getImage,
+                        icon: const Icon(Icons.add, size: 24),
+                        color: const Color.fromARGB(255, 12, 11, 11),
+                      ),
+                      const Text('No image'),
+                    ],
+                  )
+                : Image.file(_image!),
+          ],
+        ));
+  }
+}
 
 //2:image pricker do 1 first
