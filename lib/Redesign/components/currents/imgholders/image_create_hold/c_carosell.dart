1150633085cd734b7a3572a5a03f89dadee30728
Bronziel Gold <morgangiermogrimes@gmail.com>
2024-03-21 13:18:31 +0100

    works but need to fix overflow etc

--- lib/Redesign/components/currents/imgholders/image_create_hold/c_carosell.dart
+++ lib/Redesign/components/currents/imgholders/image_create_hold/c_carosell.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import '../normal/reedoneImage.dart';
 import '../normal/smallimages.dart';
+import 'package:cross_file/cross_file.dart';
+import 'dart:typed_data';
 
-class CreateMainImagePlusBoxes extends StatelessWidget {
-  const CreateMainImagePlusBoxes({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox(
-      child: Row(
-        children: [
-          CreateCarousell(),
-        ],
-      ),
-    );
-  }
-}
 //working on my new carousel
 //need to make it act like it fecthes data.
 
@@ -30,14 +15,17 @@ class CreateImagePathstring {
 }
 
 class CreateCarousell extends StatefulWidget {
-  const CreateCarousell({super.key});
+  final List<XFile> images;
+  const CreateCarousell({
+    required this.images,
+    super.key,
+  });
 
   @override
   State<CreateCarousell> createState() => _CarousellState();
 }
 
 class _CarousellState extends State<CreateCarousell> {
-  List<CreateImagePathstring> imagePathlist = [];
   //adding controllers etc
   final CarouselController primaryCarousellController = CarouselController();
   int currentPrimaryIndex = 0;
@@ -49,16 +37,20 @@ class _CarousellState extends State<CreateCarousell> {
       child: Row(
         children: [
           CarouselSlider.builder(
-            itemCount: imagePathlist.length,
-            itemBuilder: (
-              context,
-              index,
-              realIndex,
-            ) {
-              //definerar listan av imagepath så det kan användas
-              final imagePath = imagePathlist[index].imagePath;
-              //returanrar min widget som håller blikden etc
-              return BigImageBoxes1(imagePath: imagePath);
+            itemCount: widget.images.length,
+            itemBuilder: (context, index, realIndex) {
+              return FutureBuilder<Uint8List>(
+                future: widget.images[index].readAsBytes(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.data != null) {
+                    return BigImageBoxes2(imageData: snapshot.data!);
+                  } else if (snapshot.hasError) {
+                    return Text('Error loading image');
+                  }
+                  return CircularProgressIndicator(); // Or some other placeholder
+                },
+              );
             },
             options: CarouselOptions(
               autoPlay: true,
@@ -80,7 +72,7 @@ class _CarousellState extends State<CreateCarousell> {
             width: 40,
           ),
           Sidecolumn(
-            imagePathlist: imagePathlist,
+            images: widget.images,
             primaryCarousellController: primaryCarousellController,
             current: currentPrimaryIndex,
           )
@@ -91,32 +83,56 @@ class _CarousellState extends State<CreateCarousell> {
 }
 
 class Sidecolumn extends StatelessWidget {
-  const Sidecolumn(
-      {super.key,
-      required this.imagePathlist,
-      required this.primaryCarousellController,
-      required this.current});
-
-  final List<CreateImagePathstring> imagePathlist;
+  final List<XFile> images; // Updated to use XFile directly
   final CarouselController primaryCarousellController;
   final int current;
+
+  const Sidecolumn({
+    super.key,
+    required this.images,
+    required this.primaryCarousellController,
+    required this.current,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: imagePathlist.asMap().entries.map((entry) {
+      children: images.asMap().entries.map((entry) {
         bool isCurrent = current == entry.key;
         return GestureDetector(
           onTap: () => primaryCarousellController.animateToPage(entry.key),
           child: Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Container(
-              decoration: isCurrent ? Decoration.purpledecoration() : null,
-              margin: const EdgeInsets.symmetric(horizontal: 4.0),
-              child: Previewbox(
-                imagePath: entry.value.imagePath,
-              ),
+            child: FutureBuilder<Uint8List>(
+              future: entry.value.readAsBytes(),
+              builder:
+                  (BuildContext context, AsyncSnapshot<Uint8List> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done &&
+                    snapshot.data != null) {
+                  return Container(
+                    decoration: isCurrent
+                        ? BoxDecoration(
+                            border:
+                                Border.all(color: Colors.purple, width: 2.0),
+                            borderRadius: BorderRadius.circular(12),
+                          )
+                        : null,
+                    margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                    // Assuming PreviewBox takes imageData directly for the thumbnail.
+                    child: Image.memory(
+                      snapshot.data!,
+                      width:
+                          100, // Example width, adjust based on your UI needs
+                      height: 100, // Example height, adjust as necessary
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('Error loading image');
+                }
+                return CircularProgressIndicator(); // Placeholder while loading
+              },
             ),
           ),
         );
@@ -133,4 +149,64 @@ class Decoration {
           12), // Slightly larger than the card's border radius
     );
   }
+}
+
+class BigImageBoxes2 extends StatelessWidget {
+  final Uint8List imageData;
+
+  const BigImageBoxes2({required this.imageData, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        child: ConstrainedBox(
+      constraints: const BoxConstraints(
+        maxHeight: 500,
+        maxWidth: 1000,
+      ),
+      child: HeadlinerImage2(imageData: imageData),
+    ));
+  }
+}
+
+class HeadlinerImage2 extends StatelessWidget {
+  final Uint8List imageData;
+  const HeadlinerImage2({
+    required this.imageData,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: Card(
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            // Assuming this is a function returning a ShapeBorder
+            child: Image.memory(
+              imageData,
+              fit: BoxFit.cover,
+            )),
+      ),
+    );
+  }
+}
+
+class CustomASpectratios2 {
+  static double get aspectLaying => 3 / 2;
+  static double get aspectBoxy => 4 / 3;
+  static double get aspectPhone => 16 / 9;
+  static double get aspectFullbox => 1 / 1;
+}
+
+//static methods to control size etc
+class BoxTwos2 {
+  static double get maxWidth => 1000;
+  static double get maxHeight => 500;
+}
+
+class CardBoxTwos2 {
+  static double get maxWidth => 1000;
+  static double get maxHeight => 500;
 }
--- lib/Redesign/components/currents/imgholders/image_create_hold/imagepicker.dart
+++ lib/Redesign/components/currents/imgholders/image_create_hold/imagepicker.dart
@@ -4,61 +4,26 @@ import 'package:image_picker/image_picker.dart';
 import 'package:flutter/foundation.dart'; // For detecting platform
 import 'c_carosell.dart';
 
-//1:image pikcer
-class ImagePickerRedone extends StatefulWidget {
-  const ImagePickerRedone({super.key});
-
-  @override
-  State<ImagePickerRedone> createState() => _ImagePickerRedoneState();
-}
-
-class _ImagePickerRedoneState extends State<ImagePickerRedone> {
-  XFile? _image; // Changed to XFile to accommodate both web and mobile
-  List<XFile> images = [];
-
-  Future getImage() async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-
-    setState(() {
-      if (pickedFile != null) {
-        // An image was picked, handle accordingly
-        _image = pickedFile; // Direct assignment, works for both web and mobile
-      } else {
-        // No image was picked. This could be an intentional cancel or an error.
-        print('No image selected or an error occurred.');
-        // Optionally, you could set _image to null to reflect this state in your UI
-        _image = null;
-      }
-    });
+class ImagePickerUseable extends StatelessWidget {
+  final Function(List<XFile>) onImagesSelected;
+  const ImagePickerUseable({
+    required this.onImagesSelected,
+    super.key,
+  });
+  Future<void> pickImages() async {
+    final ImagePicker picker = ImagePicker();
+    final List<XFile>? pickedFiles = await picker.pickMultiImage();
+    if (pickedFiles != null && pickedFiles.isNotEmpty) {
+      onImagesSelected(pickedFiles);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration:
-            BoxDecoration(border: Border.all(color: Colors.black, width: 1)),
-        width: 800,
-        height: 300,
-        child: Row(
-          children: [
-            _image == null
-                ? Column(
-                    children: [
-                      IconButton(
-                        onPressed: getImage,
-                        icon: const Icon(Icons.add, size: 24),
-                        color: const Color.fromARGB(255, 12, 11, 11),
-                      ),
-                      const Text('No image'),
-                    ],
-                  )
-                : kIsWeb
-                    ? Image.network(_image!.path) // For web, use Image.network
-                    : Image.file(
-                        File(_image!.path)), // For mobile, use Image.file
-          ],
-        ));
+    return ElevatedButton(
+      onPressed: pickImages,
+      child: Text('Pick Images'),
+    );
   }
 }
 
@@ -85,24 +50,16 @@ class _ImagePickerManagementState extends State<ImagePickerManagement> {
   Widget build(BuildContext context) {
     return SizedBox(
       child: images.isEmpty
-          ? const ImagePickerRedone()
-          : const Row(
-              children: [CreateCarousell()],
+          ? ImagePickerUseable(
+              onImagesSelected: addImages,
+            )
+          : Row(
+              children: [
+                CreateCarousell(
+                  images: images,
+                )
+              ],
             ),
     );
-  }
-}
-
-class ImagePickerCarusell extends StatefulWidget {
-  const ImagePickerCarusell({super.key});
-
-  @override
-  State<ImagePickerCarusell> createState() => _ImagePickerCarusellState();
-}
-
-class _ImagePickerCarusellState extends State<ImagePickerCarusell> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
